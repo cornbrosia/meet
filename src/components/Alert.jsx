6c306fa2217488be611a6c58f3047ec98c963e92
rbenwatson <rbenwatson@gmail.com>
2025-01-12 21:36:35 -0500

    styiling

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,54 +1,27 @@
 // src/components/Alert.jsx
 
 import { Component } from 'react';
+import '../App.css'; // Import the existing App.css
 
 class Alert extends Component {
-  constructor(props) {
-    super(props);
-    this.color = null;
-    this.bgColor = null;
-  }
-
-  getStyle = () => {
-    return {
-      color: this.color,
-      backgroundColor: this.bgColor,
-      borderWidth: "2px",
-      borderStyle: "solid",
-      fontWeight: "bold",
-      borderRadius: "7px",
-      borderColor: this.color,
-      textAlign: "center",
-      fontSize: "14px",
-      margin: "10px 0",
-      padding: "10px",
-      width: "100%",
-      boxSizing: "border-box",
-    };
-  };
-
   render() {
     return (
-      <div className="Alert">
-        <p style={this.getStyle()}>{this.props.text}</p>
+      <div className={`Alert ${this.props.className}`}>
+        <p>{this.props.text}</p>
       </div>
     );
   }
 }
 
 class InfoAlert extends Alert {
-  constructor(props) {
-    super(props);
-    this.color = 'rgb(0, 0, 255)'; // Blue text
-    this.bgColor = 'rgb(230, 240, 255)'; // Softer light blue background
+  render() {
+    return <div className="Alert InfoAlert">{this.props.text}</div>;
   }
 }
 
 class ErrorAlert extends Alert {
-  constructor(props) {
-    super(props);
-    this.color = 'rgb(255, 0, 0)'; // Red text
-    this.bgColor = 'rgb(255, 230, 230)'; // Light red background for contrast
+  render() {
+    return <div className="Alert ErrorAlert">{this.props.text}</div>;
   }
 }
 

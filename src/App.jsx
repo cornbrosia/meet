3e35ee32195544e271c08c58feedd869edb53d19
rbenwatson <rbenwatson@gmail.com>
2025-01-15 13:22:54 -0500

    step 3

--- src/App.jsx
+++ src/App.jsx
@@ -64,7 +64,9 @@ const App = () => {
         setCurrentNOE={setCurrentNOE}
         setErrorAlert={setErrorAlert}
       />
+      <div className="charts-container">
       <CityEventsChart allLocations={allLocations} events={events} />
+      </div>
       <EventList events={events} />
       
       
--- src/components/CityEventsChart.js
+++ src/components/CityEventsChart.js
@@ -14,13 +14,15 @@ const CityEventsChart = ({ allLocations, events }) => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    setData(getData());
-  }, [`${data}`]);
+    if (allLocations.length && events.length) {
+      setData(getData());
+    }
+  }, [allLocations, events]); 
 
   const getData = () => {
     const data = allLocations.map((location) => {
       const count = events.filter((event) => event.location === location).length
-      const city = location.split(', ')[0]
+      const city = location.split((/, | - /))[0]
       return { city, count };
     })
     return data;
@@ -32,13 +34,16 @@ const CityEventsChart = ({ allLocations, events }) => {
         margin={{
           top: 20,
           right: 20,
-          bottom: 20,
+          bottom: 60,
           left: 20,
         }}
       >
         <CartesianGrid />
-        <XAxis type="number" dataKey="city" name="City" unit="cm" />
-        <YAxis type="number" dataKey="count" name="Number of Events" unit="kg" />
+        <XAxis
+          type="category" dataKey="city" name="City"
+          angle={60} interval={0} tick={{ dx: 20, dy: 40, fontSize: 14 }}
+        />
+        <YAxis type="number" dataKey="count" name="Number of Events" />
         <Tooltip cursor={{ strokeDasharray: '3 3' }} />
         <Scatter name="A school" data={data} fill="#8884d8" />
       </ScatterChart>
